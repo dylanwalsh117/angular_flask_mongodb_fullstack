f6262b6f5c23b5fd8428d4a7f927d16bfa698fe9
dylanwalsh117 <Dylan_Walsh@Dell.com>
2021-02-26 13:29:48 +0000

    create query for edit

--- backend/application/crudQuery.py
+++ backend/application/crudQuery.py
@@ -44,3 +44,46 @@ def query_aggregate():
                 }
         }
     ]))
+
+
+# def personid_query():
+#     return list(Person.objects.aggregate([
+#         {
+#             'lookup':
+#                 {
+#                     'from': 'sex',
+#                     'localField': 'sex',
+#                     'foreignField': '_id',
+#                     'as': 'sex'
+#                 }
+#         },
+#         {
+#             '$unwind':
+#                 {
+#                     'path': '$sex'
+#                 }
+#         },
+#
+#         {
+#             'project': {
+#                 '_id': 0,
+#                 'id': {'$toString': '$_id'},
+#                 'name': 1,
+#
+#                 'sex': {
+#
+#                     'id': {'$toString': '$sex._id'},
+#                     'gender': '$sex.gender'
+#                 },
+#
+#                 'address': {
+#
+#                     'id': {'$toString': '$address._id'},
+#                     'number': '$address.number',
+#                     'street': '$address.street',
+#                     'city': '$address.city',
+#                     'eircode': '$address.eircode'
+#                 }
+#             }
+#         }
+#     ]))
